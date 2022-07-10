52f13c63bddff38e2efa778489edc4bf64644d2f
nikisha-script <nikisha_script@mail.ru>
2022-07-10 20:01:46 +0300

    4. Многопользовательские приложения. Многопоточность в Веб приложении. [#504857]
    5. Список Вакансий [#504842 #320557]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -1,5 +1,6 @@
 package ru.job4j.dreamjob.controller;
 
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import ru.job4j.dreamjob.service.CandidateService;
 import java.time.LocalDateTime;
 
 @Controller
+@ThreadSafe
 public class CandidateController {
 
     private final CandidateService store;
--- src/main/java/ru/job4j/dreamjob/service/CandidateService.java
+++ src/main/java/ru/job4j/dreamjob/service/CandidateService.java
@@ -1,5 +1,6 @@
 package ru.job4j.dreamjob.service;
 
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Service;
 import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.store.CandidateStore;
@@ -7,6 +8,7 @@ import ru.job4j.dreamjob.store.CandidateStore;
 import java.util.Collection;
 
 @Service
+@ThreadSafe
 public class CandidateService {
 
     private final CandidateStore store;
