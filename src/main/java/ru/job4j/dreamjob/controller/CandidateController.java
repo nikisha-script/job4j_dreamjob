d4fe68467c8909b7b540e056f639916723ed7caa
nikisha-script <nikisha_script@mail.ru>
2022-07-12 12:05:47 +0300

    4. Формы. Загрузка файла на сервер. [#504855]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -1,14 +1,19 @@
 package ru.job4j.dreamjob.controller;
 
 import net.jcip.annotations.ThreadSafe;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.service.CandidateService;
+
+import java.io.IOException;
 
 
 @Controller
@@ -33,7 +38,8 @@ public class CandidateController {
     }
 
     @PostMapping("/createCandidate")
-    public String createCandidate(@ModelAttribute Candidate candidate) {
+    public String createCandidate(@ModelAttribute Candidate candidate, @RequestParam("file") MultipartFile file) throws IOException {
+        candidate.setPhoto(file.getBytes());
         store.add(candidate);
         return "redirect:/candidates";
     }
@@ -47,6 +53,23 @@ public class CandidateController {
     @PostMapping("/updateCandidate")
     public String updateCandidate(@ModelAttribute Candidate candidate) {
         store.update(candidate);
+        return "redirect:/candidates";
+    }
+
+    @GetMapping("/photoCandidate/{candidateId}")
+    public ResponseEntity<Resource> download(@PathVariable("candidateId") Integer candidateId) {
+        Candidate candidate = store.findById(candidateId);
+        return ResponseEntity.ok()
+                .headers(new HttpHeaders())
+                .contentLength(candidate.getPhoto().length)
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(new ByteArrayResource(candidate.getPhoto()));
+    }
+
+    @GetMapping("/deletePhotoCandidate/{candidateId}")
+    public String deletePhoto(@PathVariable("candidateId") Integer id) {
+        Candidate candidate = store.findById(id);
+        candidate.setPhoto(null);
         return "redirect:/candidates";
     }
 
--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -1,12 +1,11 @@
 package ru.job4j.dreamjob.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Data @NoArgsConstructor @AllArgsConstructor
+@Data @NoArgsConstructor
 public class Candidate {
 
     private int id;
@@ -14,5 +13,23 @@ public class Candidate {
     private String surname;
     private String description;
     private LocalDateTime dateOfBirth;
+    private boolean visible;
+    private byte[] photo;
+
+    public Candidate(int id, String name, String surname, String description, LocalDateTime dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.description = description;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
 
 }
